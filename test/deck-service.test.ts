1f5265eaa12c34d5922925b368d53620d6ed492a
caionastu <caionastu@gmail.com>
2022-07-11 21:36:36 -0300

    fix jest config and add cache for yarn packages

--- test/deck-service.test.ts
+++ test/deck-service.test.ts
@@ -4,8 +4,22 @@ describe('some new test suite', () => {
     expect(true).toBe(true)
   })
 
-  it('should fail', () => {
-    console.log('new test')
-    expect(true).toBe(false)
-  })
+  // jest.mock('../src/repositories/impl/DeckRepository')
+
+  // const mocked = jest.mocked(DeckRepository, true)
+
+  // it('should run a new test suite with Deck Service', () => {
+  //   const deck: IDeck = {
+  //     _id: ulid(),
+  //     title: 'test title',
+  //     userId: ulid(),
+  //     description: 'test description'
+  //   }
+
+  //   mocked.findAllByUserId.mockResolvedValue([deck])
+
+  //   mocked.findAllByUserId(deck.userId)
+
+  //   expect(mocked).toHaveBeenCalledWith(deck.userId)
+  // })
 })
